1698ced48f57cc66edbc146f9dd8b5033e314782
sugey.sugey <sugey,flores@soymas.cl>
2023-07-21 19:11:51 -0400

    python0

---
+++ 00-hello.py
@@ -0,0 +1,12 @@
+# Los lenguajes de programación como Python, los podemos pensar como una simple calculadora. Tienen valores como los números y también tienen operadores como los clasicos +,-,*,/. Pero además de solo números, la mayoría de los lenguajes modernos como Python, permite también manipular textos, y otros tipos de valores para representar colores, imágenes, sonidos, etc. Pero en su esencia, no son tan diferentes a una simple calculadora.
+
+# Pyyhon además de funcionar directamente como un programa de la terminal, lo podemos utilizar en conjunto con archivos con sentencias de Python. Esto es, en otras palabras, utilizando la sintaxis de Python.
+
+2 + 4
+
+# En el caso anterior, Python efectivamente ejecutó esa operación o conputo. Pero no muestra automáticamente el resultado de cada sentencia.
+# Si queremos o necesitamos mostrar e resultado por la terminal, debemos indicarlo explicitamente utilizando una función incorporada para estos efectos.
+
+print(2+4)
+
+# Print manda el resultado a la salida estándar, que para nuestro efectos es precisamente la terminal.
---
+++ 01-text.py
@@ -0,0 +1,10 @@
+# Dijimos que Python no solo procesa números, sino que además puede procesar muy bien textos, Y realizar operaciones interesantes con ellos.
+# Para indicar que algo es texto, Python utiliza comillas dobles "" o simples.
+
+print("Hola inmundo")
+print('Hola inmundo')
+
+# Si podemos mezclar comillas simples y dobles cuando se requiera.
+print("'¡¡Oh NO!!'exclamó gokú")
+
+# Más adelante veremos otros casos en los que se debe utilizar ambos tipos de comillas.
